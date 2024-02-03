62a4a53ae57792331eda944f590dfaa6ef15e3f0
tawan-chaidee <123750199+tawan-chaidee@users.noreply.github.com>
2024-02-03 23:31:49 +0700

    Add model, fix layout and color

---
+++ lib/model/weather_model.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class WeatherHour {
+  final String hour;
+  final IconData icon;
+  final String temperature;
+
+  WeatherHour({
+    required this.hour,
+    required this.icon,
+    required this.temperature,
+  });
+}
+
+class WeatherDay {
+  final String day;
+  final String temperature;
+  final String condition;
+  final IconData icon;
+
+  WeatherDay({
+    required this.day,
+    required this.temperature,
+    required this.condition,
+    required this.icon,
+  });
+}
+
+class WeatherToday {
+  final String condition;
+  final int temperature;
+  final int feelLike;
+  final int pressure;
+  final int humidity;
+  final int precip;  // Use int for precipitation
+  final WeatherWind wind;
+
+
+  WeatherToday({
+    required this.condition,
+    required this.temperature,
+    required this.feelLike,
+    required this.pressure,
+    required this.humidity,
+    required this.precip,
+    required this.wind,
+  });
+}
+
+class WeatherWind {
+  final int windSpeed;
+  final int windDirection;
+
+  WeatherWind({
+    required this.windSpeed,
+    required this.windDirection,
+  });
+}
+
+
+
+
+
+
+
--- lib/page/weather_page.dart
+++ lib/page/weather_page.dart
@@ -1,7 +1,56 @@
 import 'package:flutter/material.dart';
+import '../model/weather_model.dart';
 
 class WeatherPage extends StatelessWidget {
-  const WeatherPage({super.key});
+  const WeatherPage({Key? key}) : super(key: key);
+  static const themeBlueGreen = Color.fromRGBO(213, 245, 243, 1);
+
+  // Mock data for tempory use
+  static List<WeatherHour> mockHourWeatherList = [
+    WeatherHour(hour: '14:00', icon: Icons.wb_sunny, temperature: '32°C'),
+    WeatherHour(hour: '15:00', icon: Icons.cloud, temperature: '31°C'),
+    WeatherHour(hour: '16:00', icon: Icons.wb_cloudy, temperature: '35°C'),
+    WeatherHour(hour: '17:00', icon: Icons.wb_cloudy, temperature: '35°C'),
+    WeatherHour(hour: '18:00', icon: Icons.wb_cloudy, temperature: '35°C'),
+  ];
+
+  // Mock data for tempory use
+  static List<WeatherDay> mockDayWeatherList = [
+    WeatherDay(
+        day: 'Monday',
+        temperature: '25°C',
+        condition: 'Sunny',
+        icon: Icons.wb_sunny),
+    WeatherDay(
+        day: 'Tuesday',
+        temperature: '22°C',
+        condition: 'Cloudy',
+        icon: Icons.cloud),
+    WeatherDay(
+        day: 'Wednesday',
+        temperature: '28°C',
+        condition: 'Partly Cloudy',
+        icon: Icons.wb_cloudy),
+      WeatherDay(
+        day: 'Thursday',
+        temperature: '28°C',
+        condition: 'Partly Cloudy',
+        icon: Icons.wb_cloudy),
+  ];
+
+  // Mock data for tempory use
+  static WeatherToday todayWeather = WeatherToday(
+    condition: 'Sunny',
+    temperature: 25,
+    feelLike: 44,
+    pressure: 1013,
+    humidity: 60,
+    precip: 0,
+    wind: WeatherWind(
+      windSpeed: 10,
+      windDirection: 45,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -11,146 +60,237 @@ class WeatherPage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          //Top weather banner
+          weatherBanner(),
           Container(
             width: 400,
-            height: 300,
-            color: Color.fromARGB(255, 225, 255, 0),
+            height: 220,
+            // color: const Color.fromARGB(255, 225, 255, 0),
             child: Center(
               child: Wrap(
                 alignment: WrapAlignment.center,
                 spacing: 10.0,
                 runSpacing: 10.0,
-                //Top info part
+                //Middle info part
                 children: [
-                  _weather_box(100, 100, Colors.blue, 15.0, 'test', 'test'),
-                  _weather_box(100, 100, Colors.green, 20.0, 'test', 'test'),
-                  _weather_box(200, 100, Color.fromARGB(255, 250, 0, 88), 20.0,
-                      '', '', Text('test')),
+                  _weatherBox(
+                    150,
+                    100,
+                    color: themeBlueGreen,
+                    borderRadius: 15.0,
+                    title: 'Feel Like',
+                    subtitle1: todayWeather.feelLike.toString() + '°C',
+                  ),
+                  _weatherBox(150, 100,
+                      color: themeBlueGreen,
+                      borderRadius: 20.0,
+                      title: 'Pressure',
+                      subtitle1: todayWeather.pressure.toString()),
+                  _weatherBox(
+                    300,
+                    100,
+                    color: themeBlueGreen,
+                    borderRadius: 20.0,
+                    title: '',
+                    subtitle1: '',
+                    customChild: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _weatherBox(
+                          100,
+                          100,
+                          borderRadius: 0,
+                          title: "Wind",
+                          subtitle1: 'Speed: ${todayWeather.wind.windSpeed}',
+                          subtitle2: 'Direction: ',
+                        ),
+                        _weatherBox(
+                          140,
+                          100,
+                          borderRadius: 0,
+                          title: '',
+                          subtitle1: '',
+                          //Arrow Direction to be implement later
+                          customChild: const Icon(
+                            Icons.arrow_forward,
+                            size: 48,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
-          //Bottom weather Table part
-          _weather_box(400, 250, Color.fromARGB(255, 242, 0, 255), 15, '', '', _weatherTable()),
-          _weather_box(400, 120, Colors.blue, 15, '', '', _hourWeatherTable()),
+          //Bottom hourly weather Table part
+          _weatherBox(
+            400,
+            120,
+            color: Colors.transparent,
+            borderRadius: 15,
+            title: '',
+            subtitle1: '',
+            customChild: _hourWeatherTable(mockHourWeatherList),
+          ),
+          //Very bottom day weather Table part
+          _weatherBox(
+            400,
+            260,
+            color: themeBlueGreen,
+            borderRadius: 0,
+            title: '',
+            subtitle1: '',
+            customChild: _weatherTable(mockDayWeatherList),
+          ),
         ],
       ),
     );
   }
 
-  // Roundy box with title and subtitle, can take widget as child if
-  // "customWidget" parameter is provided
-  Widget _weather_box(
+  Widget weatherBanner() {
+    return Container(
+      width: 400,
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            child: const Icon(
+              Icons.wb_cloudy,
+              size: 52,
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            child: _weatherBox(
+              150,
+              100,
+              borderRadius: 0,
+              title: 'BangKok',
+              subtitle1: '25°C',
+            ),
+          ),
+          Container(
+              width: 100,
+              height: 100,
+              //Custom Weather text depend on situtation to be add later
+              child: _weatherBox(150, 100,
+                  title: "Sunny",
+                  subtitle1: todayWeather.humidity.toString(),
+                  subtitle2: todayWeather.precip.toString())),
+        ],
+      ),
+    );
+  }
+
+  // Roundy box with title and subtitle
+  // Would display second subtitle or customWidget if provided
+  // Also optionaly take color as parameter
+  Widget _weatherBox(
     double width,
-    double height,
-    Color color,
-    double borderRadius,
-    String title,
-    String subtitle, [
-    Widget? customWidget,
-  ]) {
+    double height, {
+    Color? color,
+    double borderRadius = 0,
+    required String title,
+    required String subtitle1,
+    String? subtitle2,
+    Widget? customChild,
+  }) {
     return Container(
       width: width,
       height: height,
       decoration: BoxDecoration(
-        color: color,
+        color: color ?? Colors.transparent,
         borderRadius: BorderRadius.circular(borderRadius),
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          if (customWidget != null) customWidget,
-          if (customWidget == null)
+          if (customChild != null) customChild,
+          if (customChild == null)
             Column(
               children: [
                 Text(
                   title,
                   style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                const SizedBox(height: 8),
                 Text(
-                  subtitle,
+                  subtitle1,
                   style: const TextStyle(fontSize: 16),
                 ),
+                if (subtitle2 != null)
+                  Container(
+                    margin: const EdgeInsets.only(top: 0),
+                    child: Text(
+                      subtitle2,
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ),
               ],
             ),
         ],
       ),
     );
   }
-}
 
-Widget _weatherTable() {
-  List<Map<String, dynamic>> weatherData = [
-    {
-      'day': 'Monday',
-      'temperature': '25°C',
-      'condition': 'Sunny',
-      'icon': Icons.wb_sunny
-    },
-    {
-      'day': 'Tuesday',
-      'temperature': '22°C',
-      'condition': 'Cloudy',
-      'icon': Icons.cloud
-    },
-    {
-      'day': 'Wednesday',
-      'temperature': '28°C',
-      'condition': 'Partly Cloudy',
-      'icon': Icons.wb_cloudy
-    },
-  ];
-
-  return DataTable(
-    columns: [
-      DataColumn(label: Text('Day')),
-      DataColumn(label: Text('Temperature')),
-      DataColumn(label: Text('Condition')),
-    ],
-    rows: weatherData
-        .map(
-          (data) => DataRow(
-            cells: [
-              DataCell(Text(data['day'])),
-              DataCell(Text(data['temperature'])),
-              DataCell(Icon(data['icon'], color: Colors.pink, size: 24.0)),
-            ],
-          ),
-        )
-        .toList(),
-  );
-}
-
-Widget _hourWeatherTable() {
-  List<Map<String, dynamic>> weatherData = [
-    {'hour': '14:00', 'icon': Icons.wb_sunny, 'temperature': '32°C'},
-    {'hour': '15:00', 'icon': Icons.cloud, 'temperature': '31°C'},
-    {'hour': '16:00', 'icon': Icons.wb_cloudy, 'temperature': '35°C'},
-  ];
-
-  return Container(
-    padding: EdgeInsets.all(16.0),
-    child: Center(
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: weatherData.map((data) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(data['hour']),
-                SizedBox(height: 8.0),
-                Icon(data['icon'], color: Colors.pink, size: 24.0),
-                SizedBox(height: 8.0),
-                Text(data['temperature']),
+  Widget _weatherTable(List<WeatherDay> weatherData) {
+    return DataTable(
+      columns: [
+        const DataColumn(label: Text('Day')),
+        const DataColumn(label: Text('Temperature')),
+        const DataColumn(label: Text('Condition')),
+      ],
+      rows: weatherData
+          .map(
+            (data) => DataRow(
+              cells: [
+                DataCell(Text(data.day)),
+                DataCell(Text(data.temperature)),
+                DataCell(
+                  Icon(data.icon,
+                      color: const Color.fromARGB(255, 0, 0, 0), size: 24.0),
+                ),
               ],
-            );
-          }).toList(),
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  Widget _hourWeatherTable(List<WeatherHour> weatherData) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: weatherData.map((data) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 12, right: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(data.hour),
+                    const SizedBox(height: 8.0),
+                    Icon(data.icon,
+                        color: const Color.fromARGB(255, 0, 0, 0), size: 24.0),
+                    const SizedBox(height: 8.0),
+                    Text(data.temperature),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
