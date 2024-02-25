5250d1f2f6c3cb7336461f56c042be2bebbb8d11
tawan-chaidee <123750199+tawan-chaidee@users.noreply.github.com>
2024-02-25 17:54:14 +0700

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geotask/page/detail_page.dart';
+import 'package:geotask/page/weather_page.dart';
 import 'package:geotask/provider/todo_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -15,8 +15,9 @@ class MyApp extends StatelessWidget {
       create: (context) => TodoProvider(),
       child: MaterialApp(
         title: 'GeoTask',
-        home: TodoDetailPage(todoIndex: 1),
-        // WeatherPage(latitude:48.301, longitude:2.5708)
+        home: WeatherPage(latitude:48.301, longitude:2.5708),
+        // TodoDetailPage(todoIndex: 1),
+        
       ),
     );
   }
