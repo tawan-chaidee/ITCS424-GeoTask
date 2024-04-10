633f46852d8ef61e665af78de947508f27f129ad
pastelite <kana.bhong@gmail.com>
2024-04-10 16:03:12 +0700

    mapfix

--- lib/components/map.dart
+++ lib/components/map.dart
@@ -5,69 +5,87 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class StreetMap extends StatelessWidget {
+class StreetMap extends StatefulWidget {
   final List<LatLng> points;
   final double offsetFactor;
 
   StreetMap({
     super.key,
-    // this.points = const [
-    //   LatLng(51.5, -0.09),
-    //   LatLng(50, 12),
-    //   LatLng(51.5, -0.13),
-    // ],
     this.points = const [],
     this.offsetFactor = 0,
   });
 
   @override
+  State<StreetMap> createState() => _StreetMapState();
+}
+
+class _StreetMapState extends State<StreetMap> {
+  MapController? mapController;
+
+  @override
   Widget build(BuildContext context) {
-    var centralPoint = LatLng(13.74, 100.46);
+    // default data
+    var centralPoint = const LatLng(13.74, 100.46);
+    LatLngBounds? bounds;
     var zoom = 10.0;
 
-    // if (points.isNotEmpty) {
-    //   var pointsSum = [0.0, 0.0];
+    // for one point
+    if (widget.points.length == 1) {
+      centralPoint = widget.points[0];
+      zoom = 14.0;
 
-    //   var border = {
-    //     "minLat": points[0].latitude,
-    //     "maxLat": points[0].latitude,
-    //     "minLong": points[0].longitude,
-    //     "maxLong": points[0].longitude
-    //   };
+      // 50% offset because some stuff blocking the way
+      var mapHeight = 180 * widget.offsetFactor / (2 * pow(2, zoom - 2));
+      centralPoint = LatLng(centralPoint.latitude - mapHeight, centralPoint.longitude);
+    }
 
-    //   for (var point in points) {
-    //     pointsSum = [
-    //       pointsSum[0] + point.latitude,
-    //       pointsSum[1] + point.longitude
-    //     ];
-    //     if (point.latitude < border["minLat"]!) {
-    //       border["minLat"] = point.latitude;
-    //     }
-    //     if (point.latitude > border["maxLat"]!) {
-    //       border["maxLat"] = point.latitude;
-    //     }
-    //     if (point.longitude < border["minLong"]!) {
-    //       border["minLong"] = point.longitude;
-    //     }
-    //     if (point.longitude > border["maxLong"]!) {
-    //       border["maxLong"] = point.longitude;
-    //     }
-    //   }
+    // for two+ points
+    if (widget.points.length > 1) {
+      var border = {
+        "minLat": widget.points[0].latitude,
+        "maxLat": widget.points[0].latitude,
+        "minLong": widget.points[0].longitude,
+        "maxLong": widget.points[0].longitude
+      };
 
-    //   // calculate the central point and zoom using some math
-    //   centralPoint =
-    //       LatLng(pointsSum[0] / points.length, pointsSum[1] / points.length);
-    //   var maxDistance = max(border["maxLat"]! - border["minLat"]!,
-    //       border["maxLong"]! - border["minLong"]!);
-    //   zoom = log(180 / maxDistance) / log(2) + 1;
-    // }
+      for (var point in widget.points) {
+        if (point.latitude < border["minLat"]!) {
+          border["minLat"] = point.latitude;
+        }
+        if (point.latitude > border["maxLat"]!) {
+          border["maxLat"] = point.latitude;
+        }
+        if (point.longitude < border["minLong"]!) {
+          border["minLong"] = point.longitude;
+        }
+        if (point.longitude > border["maxLong"]!) {
+          border["maxLong"] = point.longitude;
+        }
+      }
 
-    // // 50% offset because some stuff blocking the way
-    // var mapHeight = 180 * offsetFactor / (2 * pow(2, zoom - 2));
-    // centralPoint =
-    //     LatLng(centralPoint.latitude - mapHeight, centralPoint.longitude);
+     centralPoint = LatLng(
+          (border["maxLat"]! + border["minLat"]!) / 2,
+          (border["maxLong"]! + border["minLong"]!) / 2
+      );
+      var maxDistance = max(
+          border["maxLat"]! - border["minLat"]!, border["maxLong"]! - border["minLong"]!);
+      zoom = log(360 / maxDistance) / log(2) + 2;
+
+      // 50% offset because some stuff blocking the way
+      var mapHeight = 180 * widget.offsetFactor / (2 * pow(2, zoom - 2));
+      centralPoint =
+          LatLng(centralPoint.latitude - mapHeight, centralPoint.longitude);
+    }
+
+    // create map controller if not exists, move if exists
+    if (mapController == null) {
+      mapController = MapController();
+    } else {
+      mapController!.move(centralPoint, zoom);
+    }
 
     return FlutterMap(
+      mapController: mapController,
       options: MapOptions(
         initialCenter: centralPoint,
         initialZoom: zoom,
@@ -79,7 +97,7 @@ class StreetMap extends StatelessWidget {
         ),
         MarkerLayer(
             alignment: Alignment.topCenter,
-            markers: points.map((point) {
+            markers: widget.points.map((point) {
               return Marker(
                 width: 40.0,
                 height: 40.0,
@@ -105,62 +123,5 @@ class StreetMap extends StatelessWidget {
         )
       ],
     );
-  }
-}
-
-class MarkerLayerBuilder extends StatelessWidget {
-  final List<LatLng> markers;
-
-  MarkerLayerBuilder({super.key, this.markers = const []});
-
-  @override
-  Widget build(BuildContext context) {
-    if (markers.isEmpty) {
-      return MarkerLayer(markers: []);
-    }
-
-    var border = {
-      "minLat": markers[0].latitude,
-      "maxLat": markers[0].latitude,
-      "minLong": markers[0].longitude,
-      "maxLong": markers[0].longitude
-    };
-
-    // need to use array because LatLng will thrown error if the number is above 90
-    var pointsSum = [0.0, 0.0];
-    for (var point in markers) {
-      pointsSum = [
-        pointsSum[0] + point.latitude,
-        pointsSum[1] + point.longitude
-      ];
-      if (point.latitude < border["minLat"]!) {
-        border["minLat"] = point.latitude;
-      }
-      if (point.latitude > border["maxLat"]!) {
-        border["maxLat"] = point.latitude;
-      }
-      if (point.longitude < border["minLong"]!) {
-        border["minLong"] = point.longitude;
-      }
-      if (point.longitude > border["maxLong"]!) {
-        border["maxLong"] = point.longitude;
-      }
-    }
-
-    var centralPoint =
-        LatLng(pointsSum[0] / markers.length, pointsSum[1] / markers.length);
-    var maxDistance = max(border["maxLat"]! - border["minLat"]!,
-        border["maxLong"]! - border["minLong"]!);
-    var zoom = log(180 / maxDistance) / log(2) + 1;
-
-    return MarkerLayer(
-        markers: markers.map((point) {
-      return Marker(
-        width: 40.0,
-        height: 40.0,
-        point: point,
-        child: Icon(Icons.location_on, size: 40.0, color: Colors.red),
-      );
-    }).toList());
   }
 }
